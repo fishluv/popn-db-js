f94c7b307bd4bf36ba228cb0d067dc847b235c5a
fishluv <110374653+fishluv@users.noreply.github.com>
2022-12-06 21:44:47 -0800

    add constants

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,13 @@
 import find from "./chart/api/find"
 import sample from "./chart/api/sample"
+import { SRAN_LEVELS } from "./SranLevel"
 
 export const Chart = {
   find,
   sample,
+}
+
+export const Constants = {
+  LEVELS: [...Array(50).keys()].map(i => i + 1), // 1 to 50, inclusive
+  SRAN_LEVELS,
 }
